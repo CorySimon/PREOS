4554ae3d3ff402ef4544d4d3b700a5cdb70a28ff
Cory <cory@ubuntu.ubuntu-domain>
2014-12-22 11:03:37 -0800

    Made Pythonic

--- PREOS.py
+++ PREOS.py
@@ -4,29 +4,31 @@ import matplotlib
 import sys
 from scipy.optimize import newton
 
-###
-#		PARAMETERS FOR MOLECULE
-###
+
+"""
+Enter parameters for the molecule here
+"""
 molecule = 'methane'
-Tc = -82.59 + 273.15 # Critical temperature, K
-Pc = 45.99 # Critical pressure, bar
-omega = 0.011 # accentric factor
-R = 8.314e-5 # universal gas constant, m3-bar/K-mol
+Tc = -82.59 + 273.15  # Critical temperature, K
+Pc = 45.99  # Critical pressure, bar
+omega = 0.011  # accentric factor
+R = 8.314e-5  # universal gas constant, m3-bar/K-mol
 print "Molecule: %s. Critical Temp = %.1f K. Critical Pressure = %.1f bar." % (molecule, Tc, Pc)
 
-###
-#		GET CONDITIONS FROM INPUT
-###
+"""
+Take in arguments temperature in Kelvin and pressure in bar
+"""
 if len(sys.argv) != 3:
-	sys.exit('Run program as:\npython PREOS.py T_in_K P_in_bar')
+    sys.exit('Run program as:\npython PREOS.py T_in_K P_in_bar')
 T = float(sys.argv[1])
 P = float(sys.argv[2])
 
-###
-#		PREOS, BUILD CUBIC TO SOLVE FOR COMPRESSIBILITY
-#   http://en.wikipedia.org/wiki/Equation_of_state#Peng.E2.80.93Robinson_equation_of_state
-###
-Tr = T / Tc
+"""
+Peng-Robinson equation of state: build cubic polynomial to solve for compressibility
+http://en.wikipedia.org/wiki/Equation_of_state#Peng.E2.80.93Robinson_equation_of_state
+"""
+# build params in PREOS
+Tr = T / Tc  # reduced temperature
 a = 0.457235 * R**2 * Tc**2 / Pc
 b = 0.0777961 * R * Tc /Pc
 kappa = 0.37464 + 1.54226 * omega - 0.26992 * omega**2
@@ -34,12 +36,17 @@ alpha = ( 1 + kappa * (1 - np.sqrt(Tr) ) )**2
 
 A = a * alpha * P / R**2 / T**2
 B = b * P / R /T
-## build cubic
+# build cubic polynomial
 def g(z):
-	return z**3 - (1 - B) * z**2 + (A - 2*B - 3*B**2) * z - (A * B - B**2 - B**3)
-z = newton(g,1) # compressibility factor
-rho = P / (R * T * z) 
+    return z**3 - (1 - B) * z**2 + (A - 2*B - 3*B**2) * z - (A * B - B**2 - B**3)
 
+"""
+Solve cubic polynomail for the compressibility factor
+"""
+z = newton(g,1)  # compressibility factor
+rho = P / (R * T * z)  # density
+
+# fugacity coefficient comes from an integration
 fugacity_coeff = np.exp(z - 1 - np.log(z - B) - A / np.sqrt(8) / B * np.log( (z + (1 + np.sqrt(2)) * B) / (z + (1 - np.sqrt(2)) * B) ))
 print "Temperature %.2f K, Pressure %.2f bar" % (T, P)
 print "Compressibility factor : %f" % z
@@ -51,12 +58,16 @@ print "Density: %f v STP/v" % ( rho * 22.4 / 1000 )
 print "IG density: %f v STP/v" % (rho * 22.4/ 1000 * z)
 print "Energy density: %f MJ/L" % (rho *16.04 /1000.0/1000.0*55.5)
 
-zz = np.linspace(0, 1.5)
+"""
+Plot the cubic equation to visualize the roots
+"""
+zz = np.linspace(0, 1.5)  # array for plotting
+
 fig = plt.figure()
-plt.plot(zz , g(zz),color='k')
+plt.plot(zz , g(zz), color='k')
 plt.xlabel('Compressibility')
 plt.ylabel('g(z)')
-plt.axvline(x = z )
-plt.axhline(y = 0 )
+plt.axvline(x=z)
+plt.axhline(y=0)
 plt.title('Visualizing roots')
 plt.show()
