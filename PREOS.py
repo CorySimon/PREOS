672cce1d6c816c2eeaf1d03dc0c6a7c11d6c1c57
Cory <cory@ubuntu.ubuntu-domain>
2015-01-09 19:20:17 -0800

    hi

--- PREOS.py
+++ PREOS.py
@@ -13,10 +13,13 @@ Tc = -82.59 + 273.15  # Critical temperature, K
 Pc = 45.99  # Critical pressure, bar
 omega = 0.011  # accentric factor
 R = 8.314e-5  # universal gas constant, m3-bar/K-mol
-print "Molecule: %s. Critical Temp = %.1f K. Critical Pressure = %.1f bar." % (molecule, Tc, Pc)
+print "Molecule: %s.\n\tCritical Temperature = %.1f K\n\tCritical Pressure = %.1f bar." % (molecule, Tc, Pc)
 
 """
-Take in arguments temperature in Kelvin and pressure in bar
+Take in arguments. Run in terminal as:
+    python PREOS.py $temperature $pressure
+   temperature in Kelvin 
+   pressure in bar
 """
 if len(sys.argv) != 3:
     sys.exit('Run program as:\npython PREOS.py T_in_K P_in_bar')
@@ -32,22 +35,25 @@ Tr = T / Tc  # reduced temperature
 a = 0.457235 * R**2 * Tc**2 / Pc
 b = 0.0777961 * R * Tc /Pc
 kappa = 0.37464 + 1.54226 * omega - 0.26992 * omega**2
-alpha = ( 1 + kappa * (1 - np.sqrt(Tr) ) )**2
+alpha = (1 + kappa * (1 - np.sqrt(Tr)))**2
 
 A = a * alpha * P / R**2 / T**2
 B = b * P / R /T
+
 # build cubic polynomial
 def g(z):
     return z**3 - (1 - B) * z**2 + (A - 2*B - 3*B**2) * z - (A * B - B**2 - B**3)
 
 """
-Solve cubic polynomail for the compressibility factor
+Solve cubic polynomial for the compressibility factor
 """
 z = newton(g,1)  # compressibility factor
 rho = P / (R * T * z)  # density
 
 # fugacity coefficient comes from an integration
 fugacity_coeff = np.exp(z - 1 - np.log(z - B) - A / np.sqrt(8) / B * np.log( (z + (1 + np.sqrt(2)) * B) / (z + (1 - np.sqrt(2)) * B) ))
+
+# print results
 print "Temperature %.2f K, Pressure %.2f bar" % (T, P)
 print "Compressibility factor : %f" % z
 print "Fugacity coefficient: %f" % fugacity_coeff
@@ -55,8 +61,7 @@ print "Fugacity at pressure %.3f bar = %.3f bar" % (P , fugacity_coeff * P)
 print "Density: %f mol/m3" % ( rho )
 print "Molar volume: %f L/mol" % ( 1/ rho * 1000 )
 print "Density: %f v STP/v" % ( rho * 22.4 / 1000 )
-print "IG density: %f v STP/v" % (rho * 22.4/ 1000 * z)
-print "Energy density: %f MJ/L" % (rho *16.04 /1000.0/1000.0*55.5)
+print "Density of ideal gas at same conditions: %f v STP/v" % (rho * 22.4/ 1000 * z)
 
 """
 Plot the cubic equation to visualize the roots
--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-Computes the fugacity coefficient, compressibility, and density of a gas using the Peng-Robinson Equation of State.
+Peng-Robinson Equation of State solver
+======================================
 
-Enter the parameters (Tc,Pc,\omega) = (critical temperature, critical pressure, accentric factor) for your molecule in the script.
+This script computes the fugacity coefficient, compressibility, and density of a gas at a particular temperature and pressure using the Peng-Robinson Equation of State.
 
-As an example, to find the properties at 298.0 K and 65.0 bar, run the script in the terminal as:
-python PREOS.py 298.0 65.0
+For your molecule, modify the script by entering the relevant parameters:
+* `Tc`: critical temperature (K)
+* `Pc`: critical pressure (bar)
+* `omega`: accentric factor
+
+Then, as an example, to find the properties at 298.0 K and 65.0 bar, run the script in the terminal as:
+`python PREOS.py 298.0 65.0`
+
+At the end, a plot will visualize the cubic polynomial to visualize the roots as a check that the correct solution was identified.
+
