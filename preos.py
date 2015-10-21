888dc0fafcd548e447bd20354aee10d2ff5ff224
Cory <cory@ubuntu.ubuntu-domain>
2015-10-20 18:28:44 -0700

    didn't finish mixture

--- preos.py
+++ preos.py
@@ -33,8 +33,11 @@ def preos(molecule, T, P, plotcubic=True, printresults=True):
     """
     Peng-Robinson equation of state (PREOS)
     http://en.wikipedia.org/wiki/Equation_of_state#Peng.E2.80.93Robinson_equation_of_state
-    :param T float: temperature in Kelvin
-    :param P float: pressure in bar
+    :param molecule: Molecule molecule of interest
+    :param T: float temperature in Kelvin
+    :param P: float pressure in bar
+    :param plotcubic: bool plot cubic polynomial in compressibility factor
+    :param printresults: bool print off properties
     """
     # build params in PREOS
     Tr = T / molecule.Tc  # reduced temperature
@@ -90,3 +93,79 @@ def preos(molecule, T, P, plotcubic=True, printresults=True):
         plt.title('Root found @ z = %.2f' % z)
         plt.show()
     return rho # mol/m3
+
+# TODO: Implement mixture in object-oriented way as well
+ # def preos_mixture(molecule_a, molecule_b, delta, T, P_total, x, plotcubic=True, printresults=True):
+ #     """
+ #     Peng-Robinson equation of state (PREOS) for a binary mixture
+ #     http://en.wikipedia.org/wiki/Equation_of_state#Peng.E2.80.93Robinson_equation_of_state
+ #     :param molecule_a: Molecule molecule 1 of interest
+ #     :param molecule_b: Molecule molecule 2 of interest
+ #     :param delta: binary interaction parameter between molecules a and b
+ #     :param T: float temperature in Kelvin
+ #     :param P_total: float total pressure in bar
+ #     :param x: array mole fractions
+ #     :param plotcubic: bool plot cubic polynomial in compressibility factor
+ #     :param printresults: bool print off properties
+ #     """
+ #     # build arrays of properties
+ #     Tc = np.array([molecule_a.Tc, molecule_b.Tc])
+ #     Pc = np.array([molecule_a.Pc, molecule_b.Pc])
+ #     omega = np.array([molecule_a.omega, molecule_b.omega])
+ # 
+ #     # build params in PREOS
+ #     Tr = T / Tc  # reduced temperature
+ #     a = 0.457235 * R**2 * Tc**2 / Pc
+ #     b = 0.0777961 * R * Tc / Pc
+ #     kappa = 0.37464 + 1.54226 * omega - 0.26992 * omega**2
+ #     alpha = (1 + kappa * (1 - np.sqrt(Tr)))**2
+ #     
+ #     # apply mixing rules
+ #     a_mix = a[0] * alpha
+ #     A = a_mix * alpha * P / R**2 / T**2
+ #     B = b * P / R / T
+ # 
+ #     # build cubic polynomial
+ #     def g(z):
+ #         """
+ #         Cubic polynomial in z from EOS. This should be zero.
+ #         :param z: float compressibility factor
+ #         """
+ #         return z**3 - (1 - B) * z**2 + (A - 2*B - 3*B**2) * z - (
+ #                 A * B - B**2 - B**3)
+ # 
+ #     # Solve cubic polynomial for the compressibility factor
+ #     z = newton(g, 1.0)  # compressibility factor
+ #     rho = P / (R * T * z)  # density
+ # 
+ #     # fugacity coefficient comes from an integration
+ #     fugacity_coeff = np.exp(z - 1 - np.log(z - B) - A / np.sqrt(8) / B * np.log(
+ #                 (z + (1 + np.sqrt(2)) * B) / (z + (1 - np.sqrt(2)) * B)))
+ # 
+ #     if printresults:
+ #         print """PREOS calculation at
+ #         \t T = %.2f K
+ #         \t P = %.2f bar""" % (T, P)
+ #         print "\tCompressibility factor : %f" % z
+ #         print "\tFugacity coefficient: %f" % fugacity_coeff
+ #         print "\tFugacity at pressure %.3f bar = %.3f bar" % (
+ #                 P, fugacity_coeff * P)
+ #         print "\tDensity: %f mol/m3" % (rho)
+ #         print "\tMolar volume: %f L/mol" % (1/ rho * 1000)
+ #         print "\tDensity: %f v STP/v" % (rho * 22.4 / 1000)
+ #         print "\tDensity of ideal gas at same conditions: %f v STP/v" % (
+ #                 rho * 22.4/ 1000 * z)
+ # 
+ #     if plotcubic:
+ #         # Plot the cubic equation to visualize the roots
+ #         zz = np.linspace(0, 1.5)  # array for plotting
+ # 
+ #         plt.figure()
+ #         plt.plot(zz, g(zz), color='k')
+ #         plt.xlabel('Compressibility, $z$')
+ #         plt.ylabel('Cubic $g(z)$')
+ #         plt.axvline(x=z)
+ #         plt.axhline(y=0)
+ #         plt.title('Root found @ z = %.2f' % z)
+ #         plt.show()
+ #     return rho # mol/m3
